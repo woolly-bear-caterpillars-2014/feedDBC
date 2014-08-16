ee32a2feec10eb2665fac1a29b9a4473091f8593
Kim Brewer <kabrewer07@gmail.com>
2014-08-16 12:07:53 -0400

    voting ajax and a few tweaks

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,9 +1,19 @@
 post "/posts/:post_id/votes" do
+  puts '*************VOTE STUFF**************'
   if current_user
-    vote = Vote.where(user_id: session[:user_id], post_id: params[:post_id]).first_or_create(value: params[:vote])
-    post = Post.find(params[:post_id])
-    vote_sum = post.vote_sum += params[:value].to_i
-    post.update(vote_sum: vote_sum)
+    p params[:vote].to_i
+    vote = Vote.where(user_id: session[:user_id], post_id: params[:post_id]).first_or_create()
+    p vote
+    p vote.value
+    unless params[:vote].to_i == vote.value
+      vote.update(value: params[:vote].to_i)
+      post = Post.find(params[:post_id])
+      vote_sum = post.vote_sum += params[:vote].to_i
+      post.update(vote_sum: vote_sum)
+    else 
+      p 'match!'
+      return ''
+    end
     if vote
       return vote.to_json
     else
--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 
+	$('button[name=vote]').on('click', function(event){
+		event.preventDefault();
+		vote = Number($(this).val());
+		action = $(this).parent().parent('form[name=post-vote]').attr('action');
+		console.log(action);
+		$.ajax(action, {
+        method: 'post',
+        data: {vote: vote},
+        dataType: 'json'
+      }
+    ).done(function(response) {
+    	if (response.post_id) {
+	    	span = $("span#post-" + response.post_id + "-vote-sum");
+	    	span.text(Number(span.text()) + vote);
+    	}
+    })
+	})
+
 });
