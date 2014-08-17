834d2781be2404664a283592ef7c1bf6439af939
Chase Blokker <chaseblokker@yahoo.com>
2014-08-17 17:18:04 -0400

    some more refactoring. has an unresolved issue with variable scope in javascript, so I just placed the functions inside the document-ready

--- public/js/application.js
+++ public/js/application.js
@@ -50,14 +50,12 @@ $(document).ready(function() {
     })
 
 
-
     // enable the user to exit the hidden submit,
     // signup, and login divs by clicking outside
     // div.  It also deactivates the background
     exitHiddenDiv(submit);
     exitHiddenDiv(signup);
     exitHiddenDiv(login);
-
 
     // click logic for signup buttop
     buttonSignup.on('click', signupButtonToggle);
@@ -69,159 +67,140 @@ $(document).ready(function() {
     $('body').on('click', 'button[name=submit_feedback]', submitButtonToggle);
 
     // signup ajax
-    buttonSignupForm.on('submit', submitSignup)
+    buttonSignupForm.on('submit', submitSignup);
 
     // login ajax
     buttonLoginForm.on('submit', submitLogin);
 
     // feedback ajax
     buttonSubmitForm.on('submit', submitFeedback);
+
+
+    function backgroundFadeIn() {
+        $('body').css({"background-color":"black"});
+        $('.container').css(
+            {"opacity": "0.5", "pointer-events": "none"}
+        );
+    }
+
+
+    function backgroundFadeOut() {
+        $('body').css({"background-color":"#20272D"});
+        $('.container').css(
+            {"opacity": "1", "pointer-events": "auto"}
+        );
+    }
+
+
+    function exitHiddenDiv(element) {
+        $(element).mouseup(function(){ return false; });
+
+        $(document).on("mouseup", function() {
+            $(element).fadeOut('fast');
+            backgroundFadeOut();
+        });
+    }
+
+
+    function loginButtonToggle() {
+        backgroundFadeIn();
+
+        signup.fadeOut('fast');
+        login.fadeIn('fast');
+    }
+
+
+    function signupButtonToggle() {
+        backgroundFadeIn();
+
+        signup.fadeIn('fast');
+        login.fadeOut('fast');
+    }
+
+
+    function submitButtonToggle() {
+        backgroundFadeIn();
+
+        signup.fadeOut('fast');
+        login.fadeOut('fast');
+        submit.fadeIn('fast');  
+    }
+
+    function injectHtmlOnLogin(response) {
+        html = '<li id="current_user">' +
+          '<button type="submit" name="profile">Welcome ' + response.username + '</button>' +
+        '</li>';
+
+        logoutButtonHTML = '<li><form name="input" action="/logout" method="get">' +
+          '<button type="submit" name="logout">Logout</button>' +
+          '</form></li>';
+        submit_button = '<li id="submit_button"><button type="button" name="submit_feedback">Submit</button></li>';
+
+        $('#submit_button').replaceWith(submit_button);
+        $('#current_user').replaceWith(html);
+        $('#signup_button').replaceWith(logoutButtonHTML);
+    }
+
+
+    function submitLogin() {
+        event.preventDefault();
+
+        $.ajax($(this).attr('action'), {
+            method: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(data) {
+                login.fadeOut('fast');
+                backgroundFadeOut();
+            },
+            error: function(xhr, status, error) {
+                login.effect("shake");
+            }
+        }).done(function(response) {
+            injectHtmlOnLogin(response);
+        })
+    }
+
+
+    function submitSignup() {
+        event.preventDefault();
+
+        $.ajax($(this).attr('action'), {
+            method: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(data) {
+                signup.fadeOut('fast');
+                backgroundFadeOut();
+            },
+            error: function(xhr, status, error) {
+                signup.effect("shake");
+            }
+        }).done(function(response) {
+            injectHtmlOnLogin(response);
+        })
+    }
+
+
+    function submitFeedback() {
+        event.preventDefault();
+
+        $.ajax($(this).attr('action'), {
+            method: $(this).attr('method'),
+            data: $(this).serialize(),
+            dataType: 'json',
+            success: function(data) {
+                submit.fadeOut('fast');
+                backgroundFadeOut();
+            },
+            error: function(xhr, status, error) {
+                submit.effect("shake");
+            }
+        }).done(function(response) {
+            console.log(response);
+        })
+    }
 });
-
-
-function backgroundFadeIn() {
-    $('body').css({"background-color":"black"});
-    $('.container').css(
-        {"opacity": "0.5", "pointer-events": "none"}
-    );
-}
-
-function backgroundFadeOut() {
-    $('body').css({"background-color":"#20272D"});
-    $('.container').css(
-        {"opacity": "1", "pointer-events": "auto"}
-    );
-}
-
-function exitHiddenDiv(element) {
-    $(element).mouseup(function(){ return false; });
-
-    $(document).on("mouseup", function() {
-        $(element).fadeOut('fast');
-        backgroundFadeOut();
-    });
-}
-
-
-
-function loginButtonToggle() {
-    var login = $('#loginStudent'),
-        signup = $('#signupStudent');
-
-    backgroundFadeIn();
-
-    $(signup).fadeOut('fast');
-    $(login).fadeIn('fast');
-}
-
-
-function signupButtonToggle() {
-    var login = $('#loginStudent'),
-        signup = $('#signupStudent');
-
-    backgroundFadeIn();
-    $(signup).fadeIn('fast');
-    $(login).fadeOut('fast');
-}
-
-
-function submitButtonToggle() {
-    var login = $('#loginStudent'),
-        signup = $('#signupStudent'),
-        submit = $('#submitFeedback');
-
-    backgroundFadeIn();
-    $(signup).fadeOut('fast');
-    $(login).fadeOut('fast');
-    $(submit).fadeIn('fast');  
-}
-
-function injectHtmlOnLogin(response) {
-    html = '<li id="current_user">' +
-      '<button type="submit" name="profile">Welcome ' + response.username + '</button>' +
-    '</li>';
-
-    logoutButtonHTML = '<li><form name="input" action="/logout" method="get">' +
-      '<button type="submit" name="logout">Logout</button>' +
-      '</form></li>';
-    submit_button = '<li id="submit_button"><button type="button" name="submit_feedback">Submit</button></li>';
-
-    $('#submit_button').replaceWith(submit_button);
-    $('#current_user').replaceWith(html);
-    $('#signup_button').replaceWith(logoutButtonHTML);
-}
-
-
-function submitLogin() {
-    var buttonLogin = $('button[name=login]'),
-        buttonLoginForm = $('form[name=login]'),
-        login = $('#loginStudent');
-
-    event.preventDefault();
-
-    $.ajax($(this).attr('action'), {
-        method: $(this).attr('method'),
-        data: $(this).serialize(),
-        dataType: 'json',
-        success: function(data) {
-            $(login).fadeOut('fast');
-            backgroundFadeOut();
-        },
-        error: function(xhr, status, error) {
-            $(login).effect("shake");
-        }
-    }).done(function(response) {
-        injectHtmlOnLogin(response);
-    })
-}
-
-
-function submitSignup() {
-    var buttonSignup = $('button[name=signup]'),
-        buttonSignupForm = $('form[name=signup]'),
-        signup = $('#signupStudent');
-
-    event.preventDefault();
-
-    $.ajax($(this).attr('action'), {
-        method: $(this).attr('method'),
-        data: $(this).serialize(),
-        dataType: 'json',
-        success: function(data) {
-            $(signup).fadeOut('fast');
-            backgroundFadeOut();
-        },
-        error: function(xhr, status, error) {
-            $(signup).effect("shake");
-        }
-    }).done(function(response) {
-        injectHtmlOnLogin(response);
-    })
-}
-
-
-function submitFeedback() {
-    var buttonSubmit = $('button[name=submit_feedback]'),
-        buttonSubmitForm = $('form[name=new_post]'),
-        submit = $('#submitFeedback');
-
-    event.preventDefault();
-
-    $.ajax($(this).attr('action'), {
-        method: $(this).attr('method'),
-        data: $(this).serialize(),
-        dataType: 'json',
-        success: function(data) {
-            $(submit).fadeOut('fast');
-            backgroundFadeOut();
-        },
-        error: function(xhr, status, error) {
-            $(submit).effect("shake");
-        }
-    }).done(function(response) {
-        console.log(response);
-    })
-}
 
 
 
