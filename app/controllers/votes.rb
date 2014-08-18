22e421fff7a0d01e4202373b9f0240bb6ed72a25
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-08-17 21:33:10 -0400

    corrected voting button, now you can undo your vote and then change it

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -1,12 +1,13 @@
 post "/posts/:post_id/votes" do
   if current_user
     vote = Vote.where(user_id: session[:user_id], post_id: params[:post_id]).first_or_create()
-    unless params[:vote].to_i == vote.value
-      vote.update(value: params[:vote].to_i)
+    if params[:vote].to_i != vote.value
+      (params[:vote].to_i == -vote.value && vote.value != 0) ? vote.update(value: 0) : vote.update(value: params[:vote].to_i)
       post = Post.find(params[:post_id])
       vote_sum = post.vote_sum += params[:vote].to_i
       post.update(vote_sum: vote_sum)
       return vote.to_json
+
     else 
       return ''
     end
