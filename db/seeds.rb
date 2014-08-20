66e7e2cfa27ff026750bc2be68dd0c8abe1818c9
Bruno Macedo Pasini <bmpasini@gmail.com>
2014-08-20 11:38:48 -0400

    seeded db

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,73 +10,78 @@ require 'faker'
   )
 end
 
-# ADMIN
-User.create(
-  username: Faker::Internet.user_name,
-  password: Faker::Internet.password,
-  email: Faker::Internet.email,
-  is_admin?: true
-)
+# # ADMIN
+# User.create(
+#   username: Faker::Internet.user_name,
+#   password: Faker::Internet.password,
+#   email: Faker::Internet.email,
+#   is_admin?: true
+# )
 
 # POSTS
-10.times do
+# 10.times do
   Post.create(
-    user_id: rand(1..11),
-    text: Faker::Lorem.sentence,
+    # user_id: rand(1..11),
+    user_id: 1,
+    text: "Free lunch everyday.",
     archive?: false,
     is_anonymous?: false
   )
-end
+# end
 
-# ANONYMOUS POSTS
-10.times do
-  Post.create(
-    user_id: rand(1..11),
-    text: Faker::Lorem.sentence,
-    archive?: false,
-    is_anonymous?: true
-  )
-end
+# # ANONYMOUS POSTS
+# 10.times do
+#   Post.create(
+#     user_id: rand(1..11),
+#     text: Faker::Lorem.sentence,
+#     archive?: false,
+#     is_anonymous?: true
+#   )
+# end
 
-# ADMIN POST
-Post.create(
-  user_id: 11,
-  text: Faker::Lorem.sentence,
-  archive?: false,
-  is_anonymous?: false
-)
+# # ADMIN POST
+# Post.create(
+#   user_id: 11,
+#   text: Faker::Lorem.sentence,
+#   archive?: false,
+#   is_anonymous?: false
+# )
 
-# VOTES
-50.times do
-  v = rand(-1..1)
-  p = rand(1..21)
-  q = rand(15..45)
+# # VOTES
+# 50.times do
+  # v = rand(-1..1)
+  # p = rand(1..21)
+  # q = rand(15..45)
+  v = p = q = 1
   Vote.create(
-    user_id: rand(1..11),
+    # user_id: rand(1..11),
+    user_id: 1, 
     post_id: p,
     value: v
   )
   po = Post.find(p)
   vote_sum = po.vote_sum += q
   po.update(vote_sum: vote_sum)
-end
+# end
 
-# COMMENTS
-20.times do
+# # COMMENTS
+# 20.times do
   Comment.create(
-    post_id: rand(1..21),
-    user_id: rand(1..11),
+    # post_id: rand(1..21),
+    # user_id: rand(1..11),
+    post_id: 1,
+    user_id: 1,
     text: Faker::Lorem.sentence,
     is_anonymous?: false
   )
-end
+# end
 
-# ANONYMOUS COMMENTS
-20.times do
-  Comment.create(
-    post_id: rand(1..21),
-    user_id: rand(1..11),
-    text: Faker::Lorem.sentence,
-    is_anonymous?: true
-  )
-end
+# # ANONYMOUS COMMENTS
+# 20.times do
+#   Comment.create(
+#     post_id: rand(1..21),
+#     user_id: rand(1..11),
+#     text: Faker::Lorem.sentence,
+#     is_anonymous?: true
+#   )
+# end
