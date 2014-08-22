f3e55b71619a368e7a25cf6be8f0adc7ce3869e0
Kim <kabrewer07@gmail.com>
2014-08-22 15:40:43 -0400

    removing print stuff and commented code

--- app/helpers/post.rb
+++ app/helpers/post.rb
@@ -7,16 +7,10 @@ helpers do
   def voted?(post, vote_direction)
     vote = Vote.find_by(user_id: session[:user_id], post_id: post)
     if vote && vote.value == vote_direction
-      p "TRUE"
       return "class='active'"
     else
-      p "FALSE"
       return ""
     end
-    #   return "class='active'" if voted?(@post.id) && voted?(@post.id).value == 1
-    # elsif vote && vote.value == -1
-    #   return ""
-    # end
   end
 
 end
