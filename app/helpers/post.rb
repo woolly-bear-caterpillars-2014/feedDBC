164c9a396ff2b4430f25d2fff562e50afe398de3
Kim <kabrewer07@gmail.com>
2014-08-22 15:14:28 -0400

    work on buttons

--- app/helpers/post.rb
+++ app/helpers/post.rb
@@ -4,4 +4,20 @@ helpers do
     @user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
+  def voted?(post, vote_direction)
+    vote = Vote.find_by(user_id: session[:user_id], post_id: post)
+    p vote_direction == vote.value
+    if vote && vote.value == vote_direction
+      p "TRUE"
+      return "class='active'"
+    else
+      p "FALSE"
+      return ""
+    end
+    #   return "class='active'" if voted?(@post.id) && voted?(@post.id).value == 1
+    # elsif vote && vote.value == -1
+    #   return ""
+    # end
+  end
+
 end
